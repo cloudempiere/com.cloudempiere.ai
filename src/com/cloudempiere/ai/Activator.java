6ccc13b51e632cf45c780501db9a0051ce39a99f
Peter Takács <takacspeti300@gmail.com>
2025-11-14 13:52:09 +0100

    feat(AIPlugin): Initial plugin setup, CLD-1601

---
+++ src/com/cloudempiere/ai/Activator.java
@@ -0,0 +1,16 @@
+package com.cloudempiere.ai;
+
+import org.adempiere.base.Core;
+import org.adempiere.plugin.utils.Incremental2PackActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends Incremental2PackActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		Core.getMappedModelFactory().scan(context, "com.cloudempiere.searchindex.model");
+
+		super.start(context);
+	}
+
+}
