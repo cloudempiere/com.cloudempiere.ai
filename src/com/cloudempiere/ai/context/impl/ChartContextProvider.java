3ce818e6a5efa407db9e0faab0cdb686011b175c
Peter Takács <takacspeti300@gmail.com>
2025-11-19 08:39:39 +0100

    feat(AIPlugin): Remove unused import in ChartContextProvider, CLD-1601
    Deleted the unused import of MChartDatasource from ChartContextProvider.java to clean up the code.

--- src/com/cloudempiere/ai/context/impl/ChartContextProvider.java
+++ src/com/cloudempiere/ai/context/impl/ChartContextProvider.java
@@ -21,7 +21,6 @@ import java.util.logging.Level;
 
 import org.adempiere.apps.graph.model.ChartDatasource;
 import org.compiere.model.MChart;
-import org.compiere.model.MChartDatasource;
 import org.compiere.model.MChartPara;
 import org.compiere.util.CLogger;
 import org.compiere.util.DB;
