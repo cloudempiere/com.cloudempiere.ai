3d3b7552c966391a79bf33a2bfdc53243c24afc7
Peter Takács <takacspeti300@gmail.com>
2026-03-04 11:42:53 +0100

    Revert "fix(osgi): resolve GenericPO ClassCastException via DS-injected model factory"
    This reverts commit b99ad7ba5536e00f14ca289af861f8624f03e309.

--- com.cloudempiere.ai.core/src/com/cloudempiere/ai/Activator.java
+++ com.cloudempiere.ai.core/src/com/cloudempiere/ai/Activator.java
@@ -1,36 +1,28 @@
 package com.cloudempiere.ai;
 
+import org.adempiere.base.Core;
 import org.adempiere.plugin.utils.Incremental2PackActivator;
-import org.idempiere.model.IMappedModelFactory;
-import org.idempiere.process.IMappedProcessFactory;
 import org.osgi.framework.BundleContext;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
 
-@Component(immediate = true)
 public class Activator extends Incremental2PackActivator {
-    
-    @Reference(service = IMappedModelFactory.class, cardinality = ReferenceCardinality.MANDATORY)
-    private IMappedModelFactory mappedModelFactory;
-    @Reference(service = IMappedProcessFactory.class, cardinality = ReferenceCardinality.MANDATORY)
-    private IMappedProcessFactory mappedProcessFactory;
-    
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-    }
-    
-    @Activate
-    public void activate(BundleContext context) {
-    	try {
-	        mappedModelFactory.scan(context, "com.cloudempiere.ai.model");
-	        mappedProcessFactory.scan(context, "com.cloudempiere.ai.process");
-    	}
-        catch (Exception e) {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		System.out.println("=== CloudEmpiere AI Activator START ===");
+
+		try {
+			Core.getMappedModelFactory().scan(context, "com.cloudempiere.ai.model");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
-    
+
+		try {
+			Core.getMappedProcessFactory().scan(context, "com.cloudempiere.ai.process");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		super.start(context);
+	}
+
 }
